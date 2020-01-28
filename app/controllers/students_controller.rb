4a8da7c800a2f6cc07051223a52a9bd37c14dac4
Alina Kornilova <alinakornilova78@gmail.com>
2020-01-28 16:38:50 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,10 +9,15 @@ class StudentsController < ApplicationController
   end
 
   def new
+    
   end
 
   def create
-    
-  end
+  @post = Post.new
+  @post.title = params[:title]
+  @post.description = params[:description]
+  @post.save
+  redirect_to post_path(@post)
+end
 
 end
